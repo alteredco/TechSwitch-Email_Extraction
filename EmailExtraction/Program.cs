5df72a12aba30b061e99f2d1bf3e8fca948d7491
Wendy Kakuda <Wendy.Kakuda@softwire.com>
2020-02-04 11:07:46 +0000

    add readtext function

--- EmailExtraction/Program.cs
+++ EmailExtraction/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace EmailExtraction
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace EmailExtraction
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+
+            string fullText = ReadText();
+            foreach (var letter in fullText)
+            {
+                 Console.Write(letter);
+            }
+            Console.ReadLine();
+
         }
+
+        private static string ReadText()
+        {
+            string path = @"C:\Users\WenKak\Desktop\Training\EmailExtraction\scanText.txt";
+            string fullText = File.ReadAllText(path);
+            return fullText;
+        }
+        
     }
+    
 }
